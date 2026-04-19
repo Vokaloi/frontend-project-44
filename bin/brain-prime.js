1951f4ffa19243f1e203456f1a1c0181099d6e7a
Vokaloi <bulavchik.d@gmail.com>
2026-04-19 22:53:28 +0300

    Add brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { runGame } from '../src/index.js';
+import { runPrimeGame } from '../src/games/prime.js';
+
+const { gameDescription, generateRound } = runPrimeGame();
+runGame(gameDescription, generateRound);
---
+++ src/games/prime.js
@@ -0,0 +1,22 @@
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const randomNumber = getRandomNumber(1, 100);
+  const question = randomNumber.toString();
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+  
+  return { question, correctAnswer };
+};
+
+export const runPrimeGame = () => {
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  return { gameDescription, generateRound };
+};
